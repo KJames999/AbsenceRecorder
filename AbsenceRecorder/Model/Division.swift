188068cfa842f195a83ea36607fd2204191b1d5b
Killian James <james.k@etoncollege.org.uk>
2022-02-02 18:40:31 +0000

    adding students and divisions

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by James, Killian (NA) on 02/02/2022.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
